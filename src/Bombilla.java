070dfe5605d7559768489d5894e14437895ca93c
prit <ngojlia2@nigul.cide.es>
2022-11-18 22:48:31 +0100

    hhhh

--- src/Bombilla.java
+++ src/Bombilla.java
@@ -1,7 +1,7 @@
 public class Bombilla {
-    private boolean estado;
-    private int vatios;
+    private boolean estado=false;
     private String color;
+    private int vatios;
     private float horas;
 
 //creamos el constructor
@@ -11,29 +11,42 @@ public Bombilla(){
 //constructor objeto
 public Bombilla(Bombilla b){
     this.estado=b.estado;
-    this.vatios=b.vatios;
     this.color=b.color;
+    this.vatios=b.vatios;
     this.horas=b.horas;
 
 }
-public Bombilla(int vatios ,String color){
-    this.estado=false;
-    this.vatios=vatios;
+public Bombilla(String color,int vatios){
     this.color=color;
+    this.vatios=vatios;
     this.horas=10;
 }
-public Bombilla(float horas){
-
-    if (horas>0 && horas<=10){
-        this.estado=true;
+    public Bombilla(float horas,int vatios,String color){
+        this.vatios=vatios;
+        this.color=color;
+        if (horas>0 && horas<=10){
+            this.estado=true;
+        }
+        else{
+            this.estado=false;
+        }
+    }
+    //metodos y funciones
+    public String getColor(){return color;}
+    public void setColor(String color){this.color=color;}
+    public int getVatios (){return vatios;}
+    public void setVatios(int vatios){this.vatios=vatios;}
+    public float getHoras(){return horas;}
+    public void setHoras(float horas) {this.horas = horas;}
+public void recargar(int horas){
+    if(this.horas<10){
+        this.horas+=horas;
     }
     else{
-        this.estado=false;
+        this.horas=horas;
     }
 }
-public void recargar(int horas){
-    if(){
+@Override
+public String toString(){return "La bombilla esta," +estado+ " la potencia de la bombilla es, " +vatios+ " con el color, " +color+ " y una duración de " +horas;}
 
-    }
-}
 }
---
+++ src/FuncionarBombilla.java
@@ -0,0 +1,7 @@
+public class FuncionarBombilla {
+    public static void main(String[] args) {
+        Bombilla bombilla1=new Bombilla(){
+
+        }
+    }
+}
